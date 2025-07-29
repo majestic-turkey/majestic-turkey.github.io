ddda2459316b0a021802f4607a946123cb9add6c
A Majestic Turkey <sodium.smith@gmail.com>
2025-07-29 01:52:49 -0400

    Retool message.js for use in HTML

--- message/message.js
+++ message/message.js
@@ -1,3 +1,5 @@
+// Object containing soccer player data
+// This code snippet is part of a larger application that displays a random soccer player's information.
 const team = [
     {
         name: "Nicolas Hagen",
@@ -28,6 +30,11 @@ const team = [
         name: "Daniel Gazdag",
         number: 8,
         position: "MF"
+    },
+    {
+        name: "Wessam Abou Ali",
+        number: 9,
+        position: "FW"
     },
     {
         name: "Diego Rossi",
@@ -65,14 +72,39 @@ const team = [
         position: "FW"
     },
     {
+        name: "Derrick Jones",
+        number: 20,
+        position: "MF"
+    },
+    {
+        name: "Yevhen Cheberko",
+        number: 21,
+        position: "DF"
+    },
+    {
+        name: "Abraham Romero",
+        number: 22,
+        position: "GK"
+    },
+    {
         name: "Mo Farsi",
         number: 23,
         position: "DF"
     },
     {
+        name: "Evan Bush",
+        number: 24,
+        position: "GK"
+    },
+    {
         name: "Sean Zawadzki",
         number: 25,
         position: "Yes"
+    },
+    {
+        name: "Lassi Lappalainen",
+        number: 26,
+        position: "MF"
     },
     {
         name: "Max Arfsten",
@@ -85,22 +117,44 @@ const team = [
         position: "GK"
     },
     {
+        name: "Cole Mrowka",
+        number: 29,
+        position: "MF"
+    },
+    {
         name: "Steven Moreira",
         number: 31,
         position: "DF"
     },
     {
         name: "Hugo Picard",
-        number: 69,
-        position: "Incoming"
+        number: 30,
+        position: "MF"
+    },
+    {
+        name: "Stanislav Lapkes",
+        number: 41,
+        position: "GK"
+    },
+    {
+        name: "Tristan Brown",
+        number: 44,
+        position: "MF"
+    },
+    {
+        name: "Cesar Ruvalcaba",
+        number: 48,
+        position: "DF"
     }
 ];
 
+// Function to create a message with a random soccer player's information
 const createMessage = () => {
     const randNum = Math.floor(Math.random() * team.length);
     return `Also, today's soccer player is ${team[randNum].name}, who plays as a ${team[randNum].position} wearing number ${team[randNum].number}.`;
 };
 
+// Function to display the message in the HTML element with id "crew"
 const displayMessage = (msg) => {
     const crewElement = document.getElementById("crew");
     crewElement.textContent = msg;
